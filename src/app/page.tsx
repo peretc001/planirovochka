38055007cda0282e5aebf7a807b77ae3319069f3
Красовский И <peretc012@gmail.com>
2026-03-06 22:27:02 +0300

    home Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import Home from '@/modules/home'
 
-const Page = () => <Home />
+const Page = () => (
+  <Suspense>
+    <Home />
+  </Suspense>
+)
 
 export default Page
