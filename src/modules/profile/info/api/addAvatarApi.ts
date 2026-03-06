454316d7438441dae0535cc4e7ba4dfc62ec94ea
Красовский И <peretc012@gmail.com>
2026-03-07 00:36:26 +0300

    fix upload avatar

--- src/modules/profile/info/api/addAvatarApi.ts
+++ src/modules/profile/info/api/addAvatarApi.ts
@@ -1,10 +1,17 @@
+import { getToken } from '@/lib/cookie'
+
 export const addAvatarApi = async (file: File) => {
   try {
     const formData = new FormData()
     formData.append('file', file)
 
+    const token = getToken()
+
     const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/profile/avatar_upload.php', {
       body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
       method: 'POST'
     })
 
