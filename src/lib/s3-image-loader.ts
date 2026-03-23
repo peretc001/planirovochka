858111184268d9f2137d4695e02589dfbad73f8b
Красовский И <peretc012@gmail.com>
2026-03-23 20:15:02 +0300

    ts fix

--- src/lib/s3-image-loader.ts
+++ src/lib/s3-image-loader.ts
@@ -1,3 +1,10 @@
-export default function s3ImageLoader({ height, quality, src, width }) {
+interface s3ImageLoaderProps {
+  height: string
+  quality: string
+  src: string
+  width: string
+}
+
+export default function s3ImageLoader({ height, quality, src, width }: s3ImageLoaderProps) {
   return `${process.env.NEXT_PUBLIC_S3_PATH}${src}?width=${width}&height=${height}&quality=${quality || 75}`
 }
