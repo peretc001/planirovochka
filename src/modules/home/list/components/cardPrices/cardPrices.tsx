726b45c52188ef9de466fa25d7ca5a79bb3e3b31
Красовский И <peretc012@gmail.com>
2026-03-27 14:52:29 +0300

    home CardPrices sort prices

--- src/modules/home/list/components/cardPrices/cardPrices.tsx
+++ src/modules/home/list/components/cardPrices/cardPrices.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 import { useTranslations } from 'next-intl'
 
 import { IPrices, IType } from '@/shared/interfaces'
@@ -19,10 +20,25 @@ const limit = 3
 const CardPrices: FC<ICardPrices> = ({ prices, types }) => {
   const t = useTranslations('profile')
 
+  const searchParams = useSearchParams() as URLSearchParams
+  const params = new URLSearchParams(searchParams)
+  const urlTypes = params.get('types')?.split(',')
+  const urlTypesSet = new Set(urlTypes)
+
   const [count, setCount] = useState(limit)
 
+  // сортировка, сначала те что в поиске, потом все остальные
   const allowed = DESIGN_TYPES.filter((designType: IType) => types.includes(designType.value))
-  const list = allowed.slice(0, count)
+  const sortedAllowed = [...allowed].sort((a, b) => {
+    const aInUrlTypes = urlTypesSet.has(a.value)
+    const bInUrlTypes = urlTypesSet.has(b.value)
+
+    if (aInUrlTypes === bInUrlTypes) return 0
+
+    return aInUrlTypes ? -1 : 1
+  })
+
+  const list = sortedAllowed.slice(0, count)
 
   const handleShow = () => setCount(allowed.length)
   const handleHide = () => setCount(limit)
