eb2650ca03044d343b61ed7e7b92140eea991198
Красовский И <peretc012@gmail.com>
2026-03-27 14:44:56 +0300

    home CardPrices show hide limit

--- src/modules/home/list/components/cardPrices/cardPrices.tsx
+++ src/modules/home/list/components/cardPrices/cardPrices.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { useTranslations } from 'next-intl'
 
 import { IPrices, IType } from '@/shared/interfaces'
@@ -14,18 +14,40 @@ interface ICardPrices {
   readonly types: string[]
 }
 
+const limit = 3
+
 const CardPrices: FC<ICardPrices> = ({ prices, types }) => {
   const t = useTranslations('profile')
 
+  const [count, setCount] = useState(limit)
+
   const allowed = DESIGN_TYPES.filter((designType: IType) => types.includes(designType.value))
+  const list = allowed.slice(0, count)
+
+  const handleShow = () => setCount(allowed.length)
+  const handleHide = () => setCount(limit)
 
   return (
     <div className={styles.root}>
       <h3>{t('prices.caption')}</h3>
 
-      {allowed?.map(type => (
+      {list?.map(type => (
         <CardPricesItem key={type.value} label={type.label} prices={prices} value={type.value} />
       ))}
+
+      {allowed.length >= limit ? (
+        <div className={styles.more}>
+          {count === limit ? (
+            <button className={styles.show} onClick={handleShow}>
+              {t('show')}
+            </button>
+          ) : (
+            <button className={styles.show} onClick={handleHide}>
+              {t('hide')}
+            </button>
+          )}
+        </div>
+      ) : null}
     </div>
   )
 }
