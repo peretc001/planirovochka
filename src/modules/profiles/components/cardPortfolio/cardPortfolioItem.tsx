6b0e25261f294c8106c31e9be838849a565e0bc3
Красовский И <peretc012@gmail.com>
2026-04-01 21:12:50 +0300

    profiles portfolio modal

--- src/modules/profiles/components/cardPortfolio/cardPortfolioItem.tsx
+++ src/modules/profiles/components/cardPortfolio/cardPortfolioItem.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useState } from 'react'
+import { Modal } from 'antd'
 
 import { IPortfolio } from '@/shared/interfaces'
 
@@ -6,34 +7,61 @@ import { CURRENCY } from '@/constants'
 
 import numberFormatter from '@/lib/numberFormatter'
 
+import CardPortfolioModal from '@/modules/profiles/components/cardPortfolioModal/cardPortfolioModal'
+
 import styles from './cardPortfolio.module.scss'
 
 interface ICardPortfolioItem {
   readonly card: IPortfolio
 }
 
-const CardPortfolioItem: FC<ICardPortfolioItem> = ({ card }) => (
-  <div className={styles.portfolio} data-card="portfolio">
-    <picture className={styles.preview}>
-      <img src={process.env.NEXT_PUBLIC_S3_PATH + card.photos[0]} />
-      <div className={styles.count}>{card.photos.length}</div>
-    </picture>
+const CardPortfolioItem: FC<ICardPortfolioItem> = ({ card }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-    <div className={styles.content}>
-      <div className={styles.title}>{card.title}</div>
+  const handleOpenModal = () => {
+    setIsModalOpen(true)
+  }
 
-      {card.description ? (
-        <div
-          className={styles.description}
-          dangerouslySetInnerHTML={{ __html: card.description }}
-        />
-      ) : null}
+  const handleCloseModal = useCallback(() => {
+    setIsModalOpen(false)
+  }, [])
 
-      <div className={styles.price}>
-        {numberFormatter(card.price)} {CURRENCY}
+  return (
+    <>
+      <div className={styles.portfolio} data-card="portfolio" onClick={handleOpenModal}>
+        <picture className={styles.preview}>
+          <img src={process.env.NEXT_PUBLIC_S3_PATH + card.photos[0]} />
+          <div className={styles.count}>{card.photos.length}</div>
+        </picture>
+
+        <div className={styles.content}>
+          <div className={styles.title}>{card.title}</div>
+
+          {card.description ? (
+            <div
+              className={styles.description}
+              dangerouslySetInnerHTML={{ __html: card.description }}
+            />
+          ) : null}
+
+          <div className={styles.price}>
+            {numberFormatter(card.price)} {CURRENCY}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+
+      {isModalOpen ? (
+        <Modal
+          className={styles.modal}
+          footer={null}
+          open={isModalOpen}
+          onCancel={handleCloseModal}
+        >
+          <CardPortfolioModal card={card} />
+        </Modal>
+      ) : null}
+    </>
+  )
+}
 
 export default CardPortfolioItem
