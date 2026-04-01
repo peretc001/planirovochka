4b77e80d12e09d4e84d4a0c8d0e3219e918e5648
Красовский И <peretc012@gmail.com>
2026-04-01 19:17:15 +0300

    portfolio metadata

--- src/app/profile/portfolio/add/page.tsx
+++ src/app/profile/portfolio/add/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 import Add from '@/modules/profile/portfolio/add'
 
+export const metadata: Metadata = {
+  title: 'Добавить объект | ' + process.env.NEXT_PUBLIC_NAME
+}
+
 const Page = () => <Add />
 
 export default Page
--- src/app/profile/portfolio/page.tsx
+++ src/app/profile/portfolio/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Metadata } from 'next'
 
 import Portfolio from '@/modules/profile/portfolio'
+
+export const metadata: Metadata = {
+  title: 'Портфолио | ' + process.env.NEXT_PUBLIC_NAME
+}
 
 const Page = () => <Portfolio />
 
